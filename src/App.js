23b642e63d3046575bd55877d1ebc202e543a45c
Aryaman <aryamanadivyasingh@gmail.com>
2022-11-01 01:44:02 +0530

    First Commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,77 @@
-import logo from './logo.svg';
-import './App.css';
+import React,{useState,useEffect} from 'react';
+//4ff907b8
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+import Moviecard from './Moviecard.jsx';
+import './App.css';
+import  SearchIcon from './search.svg';
+
+const API_URL = 'http://www.omdbapi.com?apikey=4ff907b8';
+
+
+
+const App = () => {
+
+const [movies,setmovies]  = useState([]);  
+const [search,setsearch] = useState("");
+
+useEffect ( () => {
+
+    searchmovies("Batman");
+
+}, []);
+
+const searchmovies = async (title) => {
+        const response = await fetch(`${API_URL}&s=${title}`);
+        const data = await response.json();
+        setmovies(data.Search);
+}
+
+
+
+
+return (
+
+<div className='app'>
+
+<h1>Theatreverse</h1>
+
+
+<div className="search">
+
+<input
+
+placeholder ="Search your movies"
+    value =  {search}
+    onChange={ (e) => setsearch(e.target.value)}
+
+
+/>
+    <img
+    src = {SearchIcon}
+    alt = "Search"
+    onClick={() => searchmovies(search)}
+    />
+
+</div>
+
+
+{ movies?.length >0
+    ?(
+        <div className="container">
+           {movies.map((movie) => (
+               <Moviecard movie={movie}/>
+           ) )}
+        </div>
+    ) : (
+       <div className = "empty">
+           <h2>No Movies found</h2>
+       </div>
+    )}
+
+</div>
+
+);
 }
 
 export default App;
+
